3c161343f3f53a8e19ae6cbeb21764066060d308
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2021-03-09 14:22:39 -0800

    content: update bio

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.bio}>
-        <p>I'm a senior program manager at GitHub empowering developers with experiences to self-solve. Prior to this role, I worked as a senior support engineer where I helped customers best integrate with GitHub Actions and its APIs.</p>
-        <p>What drives me is to find opportunities to encourage, educate, and empower anyone I interact with.</p>
+        <p>I'm a <a href="https://github.com/francisfuzz" target="_blank">program manager at GitHub</a> and have had the privilege of staffing numerous ships including GitHub Actions, the GraphQL API, and Pull Request Reviews.</p>
+        <p>My working style is a blend of cultivating psychological safety, building trust, exercising curiosity, and collaborating with others to produce amazing work.</p>
+        <p>My <a href="https://www.linkedin.com/in/francis-b">professional background</a> is an eclectic mix of software engineering and customer support and I believe the secret to living is giving.</p>
       </section>
     </Layout>
   )
