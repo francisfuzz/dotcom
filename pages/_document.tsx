2d0b8755cd292d7eb63acb11ab004c5adc9ad900
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2023-04-03 21:38:52 +0000

    build: update meta val

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+    title: 'francisfuzz.com | Francis Batac',
+    description: 'Personal Website of Francis Batac',
+    image: 'https://avatars.githubusercontent.com/u/15894826?v=4',
   }
 
   return (
